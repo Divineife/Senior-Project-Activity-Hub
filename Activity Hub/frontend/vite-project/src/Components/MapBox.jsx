2c3095a8748ec0665612da62906667f836e7da28
divineife <faleyedi01@gmail.com>
2024-03-13 01:58:28 -0500

    geo data updates

--- Activity Hub/frontend/vite-project/src/Components/MapBox.jsx
+++ Activity Hub/frontend/vite-project/src/Components/MapBox.jsx
@@ -10,47 +10,50 @@ function MapBox({ eventDetails }) {
         return <p>Location information not available.</p>;
     }
   const [viewport, setViewport] = useState({
-    latitude: geometry[1],
-    longitude: geometry[0],
-    zoom: 9,
+    lat: geometry[1],
+    long: geometry[0],
+    zoom: 10,
   });
   const mapContainerRef = useRef(null);
-  const map = useRef(null);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v12",
-      center: [viewport.longitude, viewport.latitude],
+      center: [viewport.long, viewport.lat],
       zoom: viewport.zoom,
       accessToken: MAPBOX_ACCESS_TOKEN,
     });
-
     const marker = new mapboxgl.Marker()
-      .setLngLat([viewport.longitude, viewport.latitude])
+      .setLngLat([viewport.long, viewport.lat])
+      .setPopup(
+        new mapboxgl.Popup({offset: 10})
+        .setHTML(
+            `<h3>${eventDetails.eventLocation}</h3>`
+        )
+      )
       .addTo(map);
 
     map.on("move", () => {
       const newViewport = { ...viewport }; 
-      newViewport.longitude = map.getCenter().lng.toFixed(4);
-      newViewport.latitude = map.getCenter().lat.toFixed(4);
+      newViewport.long = map.getCenter().lng.toFixed(4);
+      newViewport.lat = map.getCenter().lat.toFixed(4);
       newViewport.zoom = map.getZoom().toFixed(2);
       setViewport(newViewport);
     });
 
-    // Add cleanup function to remove map instance on unmount
     return () => {map.remove(); marker.remove()};
   }, []);
 
 
 
   return (
-    <div>
+    <>
       <div className="sidebar">
-        Longitude: {viewport.longitude} | Latitude: {viewport.latitude} | Zoom: {viewport.zoom}
+        Longitude: {viewport.long} | Latitude: {viewport.lat} | Zoom: {viewport.zoom}
       </div>
       <div ref={mapContainerRef} className="map-container" />
-    </div>
+    </>
   );
 }
 
