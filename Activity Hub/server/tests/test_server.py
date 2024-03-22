68878fea5a3026571bd4dfcf50d4ef34d233036a
divineife <faleyedi01@gmail.com>
2024-03-22 14:03:45 -0500

    test

--- Activity Hub/server/tests/test_server.py
+++ Activity Hub/server/tests/test_server.py
@@ -1,7 +1,8 @@
-from ..prac_test import *
-from ..server import *
+from ..myServer.prac_test import *
+from ..myServer.server import *
 
 def test_get_all_events():
     val = sum_num(3,4)
     print(val)
     assert(sum_num(2,3) != 6)
+    pass
